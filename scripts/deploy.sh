09051aebb6677eb1247d154e130b509a0e883943
Josh Lovins <12jdlovins@gmail.com>
2017-07-19 22:31:47 -0400

    another oneee

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,26 +1,7 @@
 #!/bin/bash
 set -x
 
-if [ "$TRAVIS_BRANCH" == "development" ]
-then
-        echo "We are in the dev branch settings"
-        HOST=$dev_host
-        USER=$dev_user
-        HOME_DIR=$dev_home
-        PASS=$dev_pass
-fi
-
-if [ $TRAVIS_BRANCH == "master" ]
-then
-        echo "We are in the master branch settings"
-        HOST=$PROD_HOST
-        USER=$PROD_USER
-        HOME_DIR=$PROD_DIR
-
-        export SSHPASS=$prod_pass
-fi
-
-echo $PASS > pass.txt
-cat pass.txt
+export SSHPASS=$dev_pass
+echo $SSHPASS
 
 sshpass -f pass.txt ssh $USER@$HOST "echo 'please' > /tmp/file"
