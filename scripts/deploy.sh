0367b958db21d054069754b220fd480fab0a4ae8
Josh Lovins <12jdlovins@gmail.com>
2017-07-19 23:01:07 -0400

    ..

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
-set -x
+if [ "$TRAVIS_BRANCH" == "development" ]
+then
+        echo "We are in the dev branch settings"
+        HOST=$dev_host
+        USER_NAME=$dev_user
+        HOME_DIR=$dev_home
 
-export SSHPASS=$dev_pass
-echo $SSHPASS
+        export SSHPASS=$dev_pass
+fi
 
-eval "$(ssh-agent -s)"
-sshpass -e ssh $dev_user@$dev_host "echo 'please' > /tmp/file"
+if [ $TRAVIS_BRANCH == "master" ]
+then
+        echo "We are in the master branch settings"
+        HOST=$PROD_HOST
+        USER=$PROD_USER
+        PASSWORD=$PROD_PASSWORD
+        HOME_DIR=$PROD_DIR
+fi
+
+
+
+sshpass -e ssh -o stricthostkeychecking=no $USER_NAME@$HOST "echo 'abc' > test.file"
