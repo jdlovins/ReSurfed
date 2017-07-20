05e5a12199bbb5cd0692f561b714266a3dd21406
Josh Lovins <12jdlovins@gmail.com>
2017-07-20 01:09:06 -0400

    more updates

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,7 +4,7 @@ PROJECT_FOLDER=$1
 PROJECT_BRANCH=$2
 PROJECT_REPO=$3
 
-source ./database_config
+source ./env_variables
 
 if [ ! -d "$PROJECT_FOLDER" ]; then
     git clone $PROJECT_REPO --branch $PROJECT_BRANCH --single-branch $PROJECT_FOLDER
@@ -17,3 +17,8 @@ fi
 
 source venv/bin/activate
 pip install -r $PROJECT_FOLDER/requirements.txt
+
+cd $PROJECT_FOLDER
+
+python manage.py migrate
+python manage.py collectstatic
