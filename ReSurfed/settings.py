39cf0f5731b350b1a3868af5375b5439aa707e95
Josh Lovins <12jdlovins@gmail.com>
2017-07-20 12:36:35 -0400

    try to fix the django toolbar

--- ReSurfed/settings.py
+++ ReSurfed/settings.py
@@ -48,9 +48,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'ReSurfed.middleware.WebFactionFixes',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -147,3 +148,5 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     )
 }
+
+DEBUG_TOOLBAR_PATCH_SETTINGS = False
