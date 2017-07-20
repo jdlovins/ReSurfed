096b5757e236132fdba3018818efaae4a67134e3
Josh Lovins <12jdlovins@gmail.com>
2017-07-19 21:09:50 -0400

    i am dumb

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-TRAVIS_BRANCH="master"
-
-if [ $TRAVIS_BRANCH == "development" ]; then
-	HOST=$dev_host
-	USER=$ev_user
-	PASSWORD=$dev_pass
-	HOME_DIR=$dev_home
+if [ "$TRAVIS_BRANCH" == "development" ]
+then
+        HOST=$dev_host
+        USER=$ev_user
+        PASSWORD=$dev_pass
+        HOME_DIR=$dev_home
 fi
 
-if [ $TRAVIS_BRANCH == "master" ]; then
-	HOST=$PROD_HOST
-	USER=$PROD_USER
-	PASSWORD=$PROD_PASSWORD
-	HOME_DIR=$PROD_DIR
+if [ $TRAVIS_BRANCH == "master" ]
+then
+        HOST=$PROD_HOST
+        USER=$PROD_USER
+        PASSWORD=$PROD_PASSWORD
+        HOME_DIR=$PROD_DIR
 fi
 
 export SSHPASS=$DEPLOY_PASS
